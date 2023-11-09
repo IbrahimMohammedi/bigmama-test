3a2621492ab418cf65fcc7a8468f0a0eea8c6551
Ibrahim Mohammedi <ibrahimod77@gmail.com>
2023-11-09 21:33:46 +0100

    - Imported transformers to use the huggingface API - Created the /summarize POST end point - Added the summarize text function that uses the API

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from transformers import pipeline
 
 app = FastAPI()
 
+summarizer = pipeline("summarization")
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello, world"}
+    return {"message": "Hello, there"}
+
+@app.post("/summarize")
+async def summarize_text(text: str, max_length: int = 150):
+    try:
+        summary = summarizer(text, max_length=max_length, min_length=50, length_penalty=2.0, num_beams=4, early_stopping=True)[0]['summary_text']
+        return {"summary": summary}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error summarizing text: {str(e)}")
