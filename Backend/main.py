5c6394fb7a68971e9a13ef1778955041b38f0b3b
Ibrahim Mohammedi <ibrahimod77@gmail.com>
2023-11-10 15:48:44 +0100

    improved error handling in our /api/summarize end point

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from transformers import pipeline
 
 app = FastAPI()
+
+origins = ["http://localhost:3000"] 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= origins,  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 summarizer = pipeline("summarization")
 
@@ -9,9 +19,12 @@ summarizer = pipeline("summarization")
 def read_root():
     return {"message": "Hello, there"}
 
-@app.post("/summarize")
-async def summarize_text(text: str, max_length: int = 150):
+@app.post("/api/summarize")
+async def summarize_text(data: dict):
     try:
+        text = data.get("text", "")
+        max_length = data.get("max_length", 150)
+
         summary = summarizer(text, max_length=max_length, min_length=50, length_penalty=2.0, num_beams=4, early_stopping=True)[0]['summary_text']
         return {"summary": summary}
     except Exception as e:
