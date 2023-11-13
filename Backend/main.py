888fd990ef57440d464ca3352dce9d57f0daf844
Ibrahim Mohammedi <ibrahimod77@gmail.com>
2023-11-13 16:07:29 +0100

    added a logger function

--- Backend/main.py
+++ Backend/main.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from transformers import pipeline
 from database import MongoDB
+import logging
+
+# Set up the root logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Define a custom logging formatter
+log_formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] [%(module)s] - %(message)s")
+
+# Create a file handler to log to a file
+file_handler = logging.FileHandler("app.log")
+file_handler.setFormatter(log_formatter)
+logger.addHandler(file_handler)
 
 # Initialize FastAPI app
 app = FastAPI()
@@ -28,6 +41,7 @@ def read_root():
     return {"message": "Hello, there"}
 
 # CRUD operations:
+
 # Post a summary
 @app.post("/api/summarize")
 async def summarize_text(data: dict):
@@ -38,25 +52,29 @@ async def summarize_text(data: dict):
         if not text:
             raise HTTPException(status_code=400, detail={"error": "Please enter a text to summarize: "})
 
-        # Use the summarizer pipeline to generate a summary
-        summary = summarizer(
-            text, max_length=max_length, min_length=50, length_penalty=2.0, num_beams=4, early_stopping=True
-        )[0]['summary_text']
-
-        # Store the summary in MongoDB
+        summary = summarizer(text, max_length=max_length, min_length=50, length_penalty=2.0, num_beams=4, early_stopping=True)[0]['summary_text']
         mongo_id = await mongo_db.create_summary(user_input=text, max_length=max_length, summary=summary)
 
         return {"summary": summary, "mongo_id": mongo_id}
     except HTTPException as http_error:
+        # Log validation errors
+        logger.error(f"Validation error: {str(http_error)}")
         raise http_error
     except Exception as e:
+        # Log unhandled exceptions
+        logger.exception(f"Unhandled exception: {str(e)}")
         raise HTTPException(status_code=500, detail={"error": f"Error summarizing text: {str(e)}"})
 
 # Get all summaries
 @app.get("/api/summaries")
 async def get_summaries():
-    summaries = await mongo_db.read_summaries()
-    return summaries
+    try:
+        summaries = await mongo_db.read_summaries()
+        return summaries
+    except Exception as e:
+        # Log errors during reading summaries
+        logger.error(f"Error reading summaries: {str(e)}")
+        raise HTTPException(status_code=500, detail={"error": f"Error reading summaries: {str(e)}"})
 
 # Get a summary for a given id
 @app.get("/api/summary/{summary_id}")
@@ -66,6 +84,10 @@ async def get_summary(summary_id: str):
         return {"summary": summary}
     except HTTPException as http_error:
         return http_error
+    except Exception as e:
+        # Log errors during reading a summary by id
+        logger.error(f"Error reading summary: {str(e)}")
+        raise HTTPException(status_code=500, detail={"error": f"Error reading summary: {str(e)}", "original_exception": str(type(e))})
 
 # Update a summary for a given id
 @app.put("/api/summary/{summary_id}")
@@ -75,6 +97,10 @@ async def update_summary(summary_id: str, data: dict):
         return result
     except HTTPException as http_error:
         return http_error
+    except Exception as e:
+        # Log errors during updating a summary by id
+        logger.error(f"Error updating summary: {str(e)}")
+        raise HTTPException(status_code=500, detail={"error": f"Error updating summary: {str(e)}", "original_exception": str(type(e))})
 
 # Delete a summary for a given id   
 @app.delete("/api/summary/{summary_id}")
@@ -84,3 +110,9 @@ async def delete_summary(summary_id: str):
         return result
     except HTTPException as http_error:
         return http_error
+    except Exception as e:
+        # Log errors during deleting a summary by id
+        logger.error(f"Error deleting summary: {str(e)}")
+        raise HTTPException(status_code=500, 
+                            detail={"error": f"Error deleting summary: {str(e)}", 
+                            "original_exception": str(type(e))})
